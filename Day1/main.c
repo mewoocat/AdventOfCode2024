beabd91b02a20b8a60a21a9dbe7610d2942957a0
mewoocat <azure.horizon.4@gmail.com>
2024-12-01 20:15:38 -0600

    Not working :(

--- Day1/main.c
+++ Day1/main.c
@@ -1,15 +1,70 @@
-// Run: gcc Day1.c -o Day1; ./Day1
+// Compile and run: gcc Day1.c -o Day1; ./Day1
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void printArr(int arr[], int size){
+void printArr(int* arr, int size){
     printf("[ ");
     for (int i = 0; i < size; i++){
         printf("%d ", arr[i]);
     }
     printf("]\n");
+}
+
+int hoarePartition(int* arr, int leftBound, int rightBound){
+
+    int pivotIndex = leftBound; // Pivot around first element
+    int pivot = arr[pivotIndex];
+    int leftIndex = leftBound + 1;
+    int rightIndex = rightBound;
+    
+    // Paritioning
+    // Moving all elements lesser than the pivot to the left of it and all
+    // elements greater than the pivot to the right of it
+    while(leftIndex < rightIndex){
+        while(arr[leftIndex] < pivot && leftIndex < rightBound){
+            leftIndex++;
+        }
+        while(arr[rightIndex] > pivot && rightIndex > leftBound){
+            rightIndex--;
+        }
+        // Now the left and right indexes should either be poiting to 
+        // elements that need to be swaped or they have crossed meaning
+        // that the left and right side of the array are already sorted
+        printf("Left: %d, Right: %d\n", leftIndex, rightIndex);
+
+        // If indexes haven't crossed 
+        if(leftIndex < rightIndex){
+            // Swap value at left index with value at right index
+            int temp = arr[leftIndex];
+            arr[leftIndex] = arr[rightIndex];
+            arr[rightIndex] = temp;
+        }
+    }
+    // Swap pivot with element at right index
+    if(arr[rightIndex] < pivot){
+        int temp = arr[pivotIndex];
+        arr[pivotIndex] = arr[rightIndex];
+        arr[rightIndex] = temp;
+    }
+
+    // The new index where the pivot was placed
+    int splitIndex = rightIndex;
+
+    // Base case
+    // If array has 1 or 0 elements
+    return splitIndex;
+}
+
+// Sorts the array from lowest to highest using Quick Sort
+void sortArr(int* arr, int leftBound, int rightBound){
+    if (leftBound < rightBound){
+        int splitIndex = hoarePartition(arr, leftBound, rightBound);
+        // Recursive call 
+        sortArr(arr, leftBound, splitIndex - 1);
+        sortArr(arr, splitIndex + 1, rightBound);
+    }
 }
 
 int main(int argc, char** argv){
@@ -72,6 +127,12 @@ int main(int argc, char** argv){
     printArr(arrayA, lineCount);
     printf("Printing list B...\n");
     printArr(arrayB, lineCount);
+
+    printf("Sorting arrays\n");
+    sortArr(arrayA, 0, lineCount);
+    printf("Printing list A sorted...\n");
+    sortArr(arrayB, 0, lineCount);
+    printf("Printing list B...\n");
     
     return 0;
 }
